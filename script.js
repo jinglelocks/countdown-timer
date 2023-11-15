7b999f95bf58fc087b1d9cbda514d45b85ba3af5
Alex ThinkPad <alexander.camilleri1@gmail.com>
2023-11-15 23:37:41 +0000

    added loading spinner

--- script.js
+++ script.js
@@ -3,11 +3,14 @@ const hours = document.getElementById('hours');
 const minutes = document.getElementById('minutes');
 const seconds = document.getElementById('seconds');
 const countdown = document.getElementById('countdown');
+const year = document.getElementById('year');
+const loading = document.getElementById('loading');
 
 const currentYear = new Date().getFullYear();
 
 const goalTime = new Date(`November 18 ${currentYear} 15:00:00`);
 
+// Update countdown time
 function updateCountdown() {
   const currentTime = new Date();
   const diff = goalTime - currentTime
@@ -18,12 +21,23 @@ function updateCountdown() {
   const s = Math.floor(diff/1000) % 60;
   console.log(d,h,m,s);
 
+  // Add values to DOM
   days.innerHTML = d;
   hours.innerHTML = h < 10 ? '0' + h : h;
   minutes.innerHTML = m < 10 ? '0' + m : m;
   seconds.innerHTML = s < 10 ? '0' + s : s;
+  
+  // set background year
+  year.innerHTML = currentYear;
+
 };
 
-// Refresh
+// Show spinner before countdown
+setTimeout(() => {
+  loading.remove();
+  countdown.style.display = 'flex';
+}, 1000);
+
+// Run every second
 setInterval(updateCountdown, 1000);
 
